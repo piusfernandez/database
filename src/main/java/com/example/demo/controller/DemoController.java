379ebbb13a79d4b1aaf65135de57d846aa067e67
Pius N Fernandez <piusfernandez@hotmail.com>
2018-03-11 06:37:27 -0400

    changed return string getPGUser

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -29,7 +29,7 @@ public class DemoController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "PostgreSQL Data:" + map.toString();
+		return "PostgreSQL Data: " + map.toString();
 	}
 
 	@RequestMapping(value = "/getMYUser")
